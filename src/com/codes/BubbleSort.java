0b3b7a5ba9bc10d73c4360b5a6f50544b6fffeed
Rahul Abhishek <rabhishek@icepor.com>
2019-09-08 19:33:39 +0530

    Updating the code

--- src/com/codes/BubbleSort.java
+++ src/com/codes/BubbleSort.java
@@ -4,42 +4,43 @@ import java.util.Scanner;
 
 public class BubbleSort {
 
-	private void bSort()
-	{		
+	private void bSort() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the lenth of array: ");
-		int n= sc.nextInt();
-		
-		int [] arr = new int [n];
-		System.out.println("Enter the array ");		
-		for (int i=0; i<n; i++)
-		{
-			arr[i]=sc.nextInt();
+		int n = sc.nextInt();
+
+		int[] arr = new int[n];
+		System.out.println("Enter the array ");
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
 		}
-		
-		for (int i= 0; i<n; i++)
-		{
-			for (int j=0; j<n-1; j++)
-			{
-				if (arr[j] > arr[j+1])
-				{
-					int temp = arr[j];
-					arr[j]= arr[j+1];
-					arr[j+1] = temp;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				if (arr[i] > arr[j]) {
+//					int temp = arr[j];
+//					arr[j]= arr[j+1];
+//					arr[j+1] = temp;
+
+					arr[i] = arr[i] + arr[j];
+					arr[j] = arr[i] - arr[j];
+					arr[i] = arr[i] - arr[j];
 				}
 			}
 		}
-		System.out.print("Output is");
-		for (int i=0; i<n; i++)
-		{
+		System.out.println("Output is ");
+		for (
+
+				int i = 0; i < n; i++) {
 			System.out.print(arr[i] + " ");
 		}
-		
+
 		sc.close();
 	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-				
+
 		BubbleSort bs = new BubbleSort();
 		bs.bSort();
 	}
