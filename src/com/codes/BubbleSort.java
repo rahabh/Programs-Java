d406c7f2b1d8856e8a72f9a7cd5eb97c943af75f
Rahul Abhishek <rabhishek@icepor.com>
2019-09-08 23:30:15 +0530

    Adding optimized code of bubble sort

--- src/com/codes/BubbleSort.java
+++ src/com/codes/BubbleSort.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class BubbleSort {
 
 	private void bSort() {
+		boolean swapped = false;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the lenth of array: ");
 		int n = sc.nextInt();
@@ -16,7 +17,7 @@ public class BubbleSort {
 		}
 
 		for (int i = 0; i < n; i++) {
-			for (int j = i + 1; j < n; j++) {
+			for (int j = i + 1; j < n - 1 + 1; j++) {
 				if (arr[i] > arr[j]) {
 //					int temp = arr[j];
 //					arr[j]= arr[j+1];
@@ -25,7 +26,12 @@ public class BubbleSort {
 					arr[i] = arr[i] + arr[j];
 					arr[j] = arr[i] - arr[j];
 					arr[i] = arr[i] - arr[j];
+
+					swapped = true;
 				}
+			}
+			if (swapped == false) {
+				break;
 			}
 		}
 		System.out.println("Output is ");
