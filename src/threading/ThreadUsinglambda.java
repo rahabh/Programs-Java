c0b63e8f26ed1d10a519850747623c48c80a999a
rahabh <talktorahul.abhishek@gmail.com>
2021-04-15 19:05:14 +0530

    thread using lambda exp

--- src/threading/ThreadUsinglambda.java
+++ src/threading/ThreadUsinglambda.java
@@ -4,36 +4,56 @@ public class ThreadUsinglambda {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Runnable obj1 = new Runnable() {
-			public void run() {
-				for (int i=0;i<5;i++) {
-					System.out.println("hi" + i);
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+//		Runnable obj1 = new Runnable() {
+//			public void run()
+//		Runnable obj1 = () -> {
+//				for (int i=0;i<5;i++) {
+//					System.out.println("hi" + i);
+//					try {
+//						Thread.sleep(1000);
+//					} catch (InterruptedException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+//				}
+//			};
+		
+//		Runnable obj2 = new Runnable() {
+//			public void run() {
+//		Runnable obj2 = () -> {
+//				for (int i=0;i<5;i++) {
+//					System.out.println("hello" + i);
+//					try {
+//						Thread.sleep(1000);
+//					} catch (InterruptedException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+//				}
+//			};
+		
+		Thread t1= new Thread(() -> {
+			for (int i=0;i<5;i++) {
+				System.out.println("hi" + i);
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
 			}
-		};
-		
-		Runnable obj2 = new Runnable() {
-			public void run() {
-				for (int i=0;i<5;i++) {
-					System.out.println("hello" + i);
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+			});
+		Thread t2 = new Thread(() -> {
+			for (int i=0;i<5;i++) {
+				System.out.println("hello" + i);
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
 			}
-		};
-		
-		Thread t1= new Thread(obj1);
-		Thread t2 = new Thread(obj2);
+		});
 		
 		t1.start();
 		try {
